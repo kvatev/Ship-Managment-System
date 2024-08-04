45867759828e180f8c693e7ae6be989c1c59435d
kvatev <kvatev@codingburgas.bg>
2024-08-05 00:36:42 +0300

    Add ASP.NET project template

---
+++ ShipManagement/ShipManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ShipManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
