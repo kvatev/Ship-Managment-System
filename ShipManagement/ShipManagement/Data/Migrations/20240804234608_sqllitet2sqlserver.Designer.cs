4f40163ba7545248982db733ec2faff9b21193b0
kvatev <kvatev@codingburgas.bg>
2024-08-05 02:47:41 +0300

    Move profile identity to sqlserver

--- ShipManagement/ShipManagement/Data/Migrations/20240804234608_sqllitet2sqlserver.Designer.cs
+++ ShipManagement/ShipManagement/Data/Migrations/20240804234608_sqllitet2sqlserver.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShipManagement.Data;
 
@@ -11,9 +12,11 @@ using ShipManagement.Data;
 namespace ShipManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240804234608_sqllitet2sqlserver")]
+    partial class sqllitet2sqlserver
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
